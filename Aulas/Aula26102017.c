4a9284acd175bd109779147623dee5b5e6821f09
Raphael L. Mazanek <raphael.lapkoski@outlook.com.br>
2017-10-26 20:33:57 -0200

    Adicionado Multa Imóvel..

--- Aulas/Aula26102017.c
+++ Aulas/Aula26102017.c
@@ -4,6 +4,8 @@
 #include <locale.h>
 #include <string.h>
 
+#define VALOR_MULTA 50.00
+
 struct ficha_aluno{
     int codigo;
     char nome[50];
@@ -11,15 +13,24 @@ struct ficha_aluno{
     char email[100];
 };
 
+struct ficha_imovel{
+    int numero;
+    float valor_imposto;
+    int meses_atraso;
+    float multa;
+};
+
 int main(){
 
     int opcao;
     struct ficha_aluno aluno;
-    
+    struct ficha_imovel imovel;
+
     do{
         system("cls");
         printf("-- MENU --\n\n");
         printf("1 - Ficha Aluno\n");
+        printf("2 - Multa Imovel\n");
         printf("Opcao: ");
         scanf("%d", &opcao);
 
@@ -41,12 +52,38 @@ int main(){
                 printf("Informe o e-mail: ");
                 gets(aluno.email);
         
-                printf("\n%d", aluno.codigo);
-                printf("\n%s", aluno.nome);
-                printf("\n%d", aluno.telefone);
-                printf("\n%s\n\n", aluno.email); 
+                printf("\nDADOS ALUNO\n");
+                printf("Codigo: %d \n", aluno.codigo);
+                printf("Nome: %s \n", aluno.nome);
+                printf("Telefone: %d \n", aluno.telefone);
+                printf("E-mail: %s \n\n", aluno.email); 
 
                 printf("Pressione qualquer tecla para continuar...");
+                getch();
+            break;
+
+            case 2:
+                printf("Informe o numero do imovel: ");
+                scanf("%d", &imovel.numero);
+                fflush(stdin);
+
+                printf("Informe o valor do IPTU: ");
+                scanf("%f", &imovel.valor_imposto);
+                fflush(stdin);
+
+
+                printf("Informe os meses atrasados: ");
+                scanf("%d", &imovel.meses_atraso);
+                fflush(stdin);
+
+                imovel.multa = ((imovel.meses_atraso * VALOR_MULTA) + imovel.valor_imposto);
+
+                printf("\nDADOS IMOVEL\n");
+                printf("Numero: %d \n", imovel.numero);
+                printf("IPTU: %.2f \n", imovel.valor_imposto);
+                printf("Meses: %d \n", imovel.meses_atraso);
+                printf("Total de multa: %.2f \n\n", imovel.multa);
+                printf("\n\nPressione qualquer tecla para continuar...");
                 getch();
             break;
 
