c204d8e20273109e84af18d92879d64514691c58
Raphael L. Mazanek <raphael.lapkoski@outlook.com.br>
2017-11-13 22:18:34 -0200

    Criação do Menu de opções do Cliente.

--- Aps/aps.c
+++ Aps/aps.c
@@ -3,9 +3,34 @@
 #include <conio.h>
 #include <locale.h>
 
+void Menu();
+
 int main(){
 
-    printf("\n\n\n Ola Mundo \n\n\n");
+    Menu();
     
     system("pause");
+}
+
+//Menu de Clientes.
+void Menu(){
+
+    int opcao;
+
+    do{        
+
+        system("cls");
+        printf("======= MENU CLIENTE ======= \n\n");
+        printf("\t");
+        printf("1) Cadastrar \n");
+        printf("\t");
+        printf("2) Listar \n");
+        printf("\t");
+        printf("3) Excluir \n");
+        printf("\t");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+
+    }while(opcao != 0);
+
 }
